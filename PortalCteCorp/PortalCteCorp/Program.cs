8c6bf0eeac1fdbf82792f3ce6be7e14eab7c3873
ArielPA73 <89750061+ArielPA73@users.noreply.github.com>
2021-11-09 13:11:34 -0300

    Nuevo

---
+++ PortalCteCorp/PortalCteCorp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PortalCteCorp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.Title = "PORTAL DE AUTOGESTIÓN - CLIENTES CORPORATIVOS";
+
+
+            Console.ReadKey();
+        }
+    }
+}
